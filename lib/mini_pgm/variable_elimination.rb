ee888e17b621e4724415cac2d750ca30ede5c689
Tristan Penman <tristan@tristanpenman.com>
2022-05-30 14:23:38 +1000

    WIP variable elimination

---
+++ lib/mini_pgm/variable_elimination.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module MiniPGM
+  class VariableElimination
+    def initialize(model)
+      @model = model
+    end
+
+    def query(variables, evidence, elimination_order)
+      nil
+    end
+  end
+end
